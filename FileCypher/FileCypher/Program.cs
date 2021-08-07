8df1349b4338354ca7e175f85a89d44a0a1e3eb9
The0a <44963952+The0a@users.noreply.github.com>
2021-08-07 20:24:37 +0200

    Add files via upload

---
+++ FileCypher/FileCypher/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+namespace FileCypher
+{
+    class Program
+    {
+        
+        static void Main(string[] args) {
+            Cypher cypher = new Cypher();
+            String file = "D:/Projets visual studio/FileCypher/FileCypher/FileCypher/TextFile1.txt";
+            cypher.Cyphering(file);
+        }
+        
+            
+    }
+}
+
+    
+
